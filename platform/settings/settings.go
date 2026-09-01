90fc2b4f9820eef8098742f0250ac39844f1621f
Pavel <tessmanpavel@gmail.com>
2026-09-01 19:46:47 +0300

    platform/settings: add server listen addr

--- platform/settings/settings.go
+++ platform/settings/settings.go
@@ -2,8 +2,11 @@ package settings
 
 import "os"
 
+const defaultApiAddr = "127.0.0.1:8080"
+
 type Settings struct {
 	TgToken string
+	ApiAddr string
 }
 
 func NewSettings() Settings {
@@ -12,8 +15,15 @@ func NewSettings() Settings {
 	if len(TgToken) == 0 {
 		panic("TG_TOKEN is not set")
 	}
+
+	ApiAddr := os.Getenv("API_ADDR")
+	if len(ApiAddr) == 0 {
+		ApiAddr = defaultApiAddr
+	}
+
 	settings := Settings{
 		TgToken: TgToken,
+		ApiAddr: ApiAddr,
 	}
 	return settings
 }
