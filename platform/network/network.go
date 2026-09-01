6baf05ab45e14e730651d9110150c78bde4610a8
paveltessman <74964953+paveltessman@users.noreply.github.com>
2026-09-01 17:02:42 +0300

    platform/telegram: implement the telegram api client (#2)

--- platform/network/network.go
+++ platform/network/network.go
@@ -1,2 +1,9 @@
 // Package network provides code for outgoing network requests.
 package network
+
+type ContentType string
+
+type HTTPRequester interface {
+	GetBytes(path string) ([]byte, error)
+	PostBytes(path string, contentType ContentType, body []byte) ([]byte, error)
+}
