c5b328cf9c88a4192f5143419c67c12d71fcd670
Pavel <tessmanpavel@gmail.com>
2026-09-03 20:10:21 +0300

    pipelines/shared: introduce a signal error that execution is completed

--- pipelines/shared/runner.go
+++ pipelines/shared/runner.go
@@ -1,12 +1,21 @@
 package shared
 
-import "context"
+import (
+	"context"
+	"errors"
+)
+
+var ErrCompleted = errors.New("pass completed")
 
 type Runner[S Session] func(context.Context, S, []Handler[S]) error
 
 func Run[S Session](ctx context.Context, session S, chain []Handler[S]) error {
 	for _, handler := range chain {
-		if err := handler.Handle(ctx, session); err != nil {
+		err := handler.Handle(ctx, session)
+		if errors.Is(err, ErrCompleted) {
+			return nil
+		}
+		if err != nil {
 			return err
 		}
 	}
